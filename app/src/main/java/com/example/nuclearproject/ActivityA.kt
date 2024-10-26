2ac7cc1f3c38b3534cd85eed78b349048ffaa5e7
teg0 <abc@mail.ru>
2024-10-26 20:29:50 +0300

    dz 1 part 1 completed

--- app/src/main/java/com/example/nuclearproject/ActivityA.kt
+++ app/src/main/java/com/example/nuclearproject/ActivityA.kt
@@ -10,17 +10,24 @@ class ActivityA : ComponentActivity()
 {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val prev = savedInstanceState?.getString("ID")
         setContentView(R.layout.main)
     }
 
     fun onClicActivityB(v: View) {
         val intent = Intent(this, ActivityB::class.java)
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
         startActivity(intent)
     }
 
     fun onClickFragmentB(v: View)
     {
 
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) { // Here You have to save count value
+        outState.putString("ID", "My String")
+        super.onSaveInstanceState(outState)
     }
 
 }
--- app/src/main/java/com/example/nuclearproject/ActivityC.kt
+++ app/src/main/java/com/example/nuclearproject/ActivityC.kt
@@ -4,19 +4,18 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.activity.ComponentActivity
-import androidx.activity.enableEdgeToEdge
 
-class ActivityB : ComponentActivity()
+class ActivityC : ComponentActivity()
 {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_b)
+        setContentView(R.layout.activity_c)
     }
 
-    fun onClicActivityC(v: View) {
-        val intent = Intent(this, ActivityC::class.java)
+    fun onClicActivityA(v: View) {
+        val intent = Intent(this, ActivityA::class.java)
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
         startActivity(intent)
     }
-
 
 }
